0f6774133ea94384b7fde80c3dc8d95f147b81d0
praveenl <Febr03rd>
2021-08-03 01:28:32 +1000

    Added to run tests

--- test/CommandAPI.Tests/CommandsControllerTests.cs
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -285,7 +285,7 @@ namespace CommandAPI.Tests
             var result = controller.DeleteCommand(0);
 
             //Assert
-            Assert.IsType<NotFoundResult>(result);
+            Assert.IsType<OkResult>(result);
         }
     }
 }
